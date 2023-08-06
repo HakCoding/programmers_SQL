74646edff3e4f0d59a1213f9f7d13d803ae309ac
HakCoding <qudgkr1230@naver.com>
2023-08-06 15:20:16 +0900

    [level 1] Title: 어린 동물 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv1/59037. 어린 동물 찾기/어린 동물 찾기.sql
+++ 프로그래머스/lv1/59037. 어린 동물 찾기/어린 동물 찾기.sql
@@ -1,5 +1,3 @@
-SELECT animal_id, name
-from animal_ins
-where intake_condition not like 'Aged'
-order by ANIMAL_ID
-;
+SELECT ANIMAL_ID, NAME
+    FROM ANIMAL_INS 
+        WHERE NOT INTAKE_CONDITION=('Aged');
