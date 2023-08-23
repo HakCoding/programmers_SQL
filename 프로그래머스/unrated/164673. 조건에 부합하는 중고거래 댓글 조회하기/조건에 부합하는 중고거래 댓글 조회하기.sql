856bdb76660851662eaa608adfa5ca3f1fc12eca
HakCoding <qudgkr1230@naver.com>
2023-08-23 16:51:24 +0900

    [unrated] Title: 조건에 부합하는 중고거래 댓글 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/unrated/164673. 조건에 부합하는 중고거래 댓글 조회하기/조건에 부합하는 중고거래 댓글 조회하기.sql
+++ 프로그래머스/unrated/164673. 조건에 부합하는 중고거래 댓글 조회하기/조건에 부합하는 중고거래 댓글 조회하기.sql
@@ -1,7 +1,5 @@
-select a.TITLE, a.BOARD_ID, b.REPLY_ID, b.WRITER_ID, b.CONTENTS,
-to_char(b.CREATED_DATE, 'yyyy-mm-dd') CREATED_DATE
-from USED_GOODS_BOARD a, USED_GOODS_REPLY b
-where a.BOARD_ID = b.BOARD_ID
-and a.CREATED_DATE
-between to_date('2022-10-01', 'yyyy-mm-dd') and to_date('2022-10-31','yyyy-mm-dd')
-order by b.CREATED_DATE asc, a.TITLE asc
+SELECT b.title, b.board_id, r.reply_id, r.writer_id, r.contents, DATE_FORMAT(r.created_date,'%Y-%m-%d') AS created_date
+FROM used_goods_board AS b 
+JOIN used_goods_reply AS r ON b.board_id = r.board_id
+WHERE b.created_date LIKE "2022-10%"
+ORDER BY created_date, b.title
