148127acff6a714e1add8fa978f16ea28a81d857
HakCoding <qudgkr1230@naver.com>
2023-09-16 22:25:53 +0900

    [level 2] Title: 루시와 엘라 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv2/59046. 루시와 엘라 찾기/루시와 엘라 찾기.sql
+++ 프로그래머스/lv2/59046. 루시와 엘라 찾기/루시와 엘라 찾기.sql
@@ -1,4 +1,10 @@
-SELECT ANIMAL_ID, NAME, SEX_UPON_INTAKE
-FROM ANIMAL_INS
-WHERE NAME IN ('Lucy','Ella','Pickle','Rogan','Sabrina','Mitty')
-ORDER BY ANIMAL_ID;
+SELECT
+    animal_id ,
+    name ,
+    sex_upon_intake
+FROM
+    animal_ins
+WHERE
+    lower(name) in ('lucy' , 'ella' , 'pickle' , 
+                    'rogan' , 'sabrina','mitty')
+ORDER BY animal_id
