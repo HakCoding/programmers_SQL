ff712c4390727271ebde003a1da773e432804281
HakCoding <qudgkr1230@naver.com>
2023-10-08 22:02:32 +0900

    [level 3] Title: 오랜 기간 보호한 동물(2), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv3/59411. 오랜 기간 보호한 동물（2）/오랜 기간 보호한 동물（2）.sql
+++ 프로그래머스/lv3/59411. 오랜 기간 보호한 동물（2）/오랜 기간 보호한 동물（2）.sql
@@ -1,9 +1,6 @@
-select animal_id, name
-from (
-    SELECT i.ANIMAL_ID, i.NAME
-    from ANIMAL_INS i
-    join ANIMAL_OUTS o
-    on i.ANIMAL_ID = o.ANIMAL_ID
-    order by o.DATETIME - i.DATETIME desc
-)
-where rownum <= 2
+SELECT animal_id, ins.name 
+FROM animal_ins AS ins
+INNER JOIN animal_outs AS outs USING(animal_id)
+WHERE outs.datetime IS NOT NULL
+ORDER BY DATEDIFF(outs.datetime, ins.datetime) DESC
+LIMIT 2;
